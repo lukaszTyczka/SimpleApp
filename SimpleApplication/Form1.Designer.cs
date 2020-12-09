73d45f02f4f2d987222b46daa9e6921a44d1079d
Łukasz Tyczka <lukasz@tyczka.net>
2020-12-09 20:50:24 +0100

    Initial commit

---
+++ SimpleApplication/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace SimpleApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.AgeLabel = new System.Windows.Forms.Label();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.AgeTextBox = new System.Windows.Forms.TextBox();
+            this.CheckBTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(12, 9);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(35, 13);
+            this.NameLabel.TabIndex = 0;
+            this.NameLabel.Text = "Name";
+            // 
+            // AgeLabel
+            // 
+            this.AgeLabel.AutoSize = true;
+            this.AgeLabel.Location = new System.Drawing.Point(12, 53);
+            this.AgeLabel.Name = "AgeLabel";
+            this.AgeLabel.Size = new System.Drawing.Size(26, 13);
+            this.AgeLabel.TabIndex = 1;
+            this.AgeLabel.Text = "Age";
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ResultLabel.Location = new System.Drawing.Point(12, 142);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(0, 13);
+            this.ResultLabel.TabIndex = 2;
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(15, 26);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(231, 20);
+            this.NameTextBox.TabIndex = 3;
+            // 
+            // AgeTextBox
+            // 
+            this.AgeTextBox.Location = new System.Drawing.Point(15, 70);
+            this.AgeTextBox.Name = "AgeTextBox";
+            this.AgeTextBox.Size = new System.Drawing.Size(231, 20);
+            this.AgeTextBox.TabIndex = 4;
+            // 
+            // CheckBTN
+            // 
+            this.CheckBTN.Location = new System.Drawing.Point(15, 106);
+            this.CheckBTN.Name = "CheckBTN";
+            this.CheckBTN.Size = new System.Drawing.Size(75, 23);
+            this.CheckBTN.TabIndex = 5;
+            this.CheckBTN.Text = "Sprawdz";
+            this.CheckBTN.UseVisualStyleBackColor = true;
+            this.CheckBTN.Click += new System.EventHandler(this.CheckBTN_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 172);
+            this.Controls.Add(this.CheckBTN);
+            this.Controls.Add(this.AgeTextBox);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.AgeLabel);
+            this.Controls.Add(this.NameLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Label AgeLabel;
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.TextBox AgeTextBox;
+        private System.Windows.Forms.Button CheckBTN;
+    }
+}
+
---
+++ SimpleApplication/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBTN_Click(object sender, EventArgs e)
+        {
+            var result = int.TryParse(AgeTextBox.Text, out var age);
+            if (!result || string.IsNullOrWhiteSpace(NameTextBox.Text))
+            {
+                ResultLabel.ForeColor = Color.Red;
+                ResultLabel.Text = "Niepoprawny wiek lub brak imienia!";
+            }
+            else if(age > 17)
+            {
+                ResultLabel.ForeColor = Color.Green;
+                ResultLabel.Text = $"{NameTextBox.Text}! Proszę oto twoje piwo!";
+            }
+            else
+            {
+                ResultLabel.ForeColor = Color.Red;
+                ResultLabel.Text = $"{NameTextBox.Text}! Nie mogę podać Ci piwa jesteś niepełnoletni.";
+            }
+        }
+    }
+}
